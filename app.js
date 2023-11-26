beeb393dd536e0fa9aee9f2269128724976e20e1
soyoung <soyseo99@naver.com>
2023-11-26 18:04:45 +0900

    function syntax

--- app.js
+++ app.js
@@ -2,3 +2,5 @@
 var studentID = 12345;
 var studentName = "soyoung";
 var courseCompleted = false;
+function getStudentDetails(studentID) {
+}
--- app.ts
+++ app.ts
@@ -2,3 +2,7 @@
 let studentID:number =12345;
 let studentName:string ="soyoung"
 let courseCompleted = false;
+
+function getStudentDetails(studentID:number):void{
+
+}
