e4bb466ba952f152b17a2c041e84b9c3a58f9c86
soyoung <soyseo99@naver.com>
2023-11-27 21:14:58 +0900

    interface parameter

--- app.js
+++ app.js
@@ -2,6 +2,11 @@
 var studentID = 12345;
 var studentName = "soyoung";
 var courseCompleted = false;
+var student1 = {
+    studentID: 12345,
+    studentName: "soyoung",
+    courseCompleted: false,
+};
 function getStudentDetails(studentID) {
     return {
         studentID: 12345,
@@ -9,3 +14,7 @@ function getStudentDetails(studentID) {
         courseCompleted: false,
     };
 }
+//매개변수 타입으로도 사용가능한 interface
+function saveStudentDetails(student) {
+}
+saveStudentDetails(student1);
--- app.ts
+++ app.ts
@@ -3,9 +3,14 @@ let studentID: number = 12345;
 let studentName: string = "soyoung";
 let courseCompleted: boolean = false;
 
+let student1={
+    studentID: 12345,
+    studentName: "soyoung",
+    courseCompleted: false,
+}
 interface Student {
   studentID: number;
-  studentName: string;
+  studentName?: string; //optional
   courseCompleted: boolean;
 }
 
@@ -16,3 +21,9 @@ function getStudentDetails(studentID: number): Student {
     courseCompleted: false,
   };
 }
+
+//매개변수 타입으로도 사용가능한 interface
+function saveStudentDetails(student:Student):void{
+
+}
+saveStudentDetails(student1)
