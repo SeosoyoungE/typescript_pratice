f1ee69ecdd98ebc13db21cbd75116c8776f91e05
soyoung <soyseo99@naver.com>
2023-12-10 23:07:40 +0900

    type alias

--- app.js
+++ app.js
@@ -1,4 +1,2 @@
-//유니온 타압
-var price = 5;
-price = 'five';
-price = true;
+var price;
+var phoneNumber;
--- app.ts
+++ app.ts
@@ -1,4 +1,6 @@
-//유니온 타압
-let price: number|string=5;
-price='five';
-price=true;
+//type alias
+//반복되는 유니온 타입을 정의하여 코드정리
+type StrOrNum=string|number;
+
+let price:StrOrNum;
+let phoneNumber:StrOrNum;
