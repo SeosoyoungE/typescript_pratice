011362162f25ce39578b604e6cf5a585c383ad98
soyoung <soyseo99@naver.com>
2023-12-10 22:36:41 +0900

    any 타입

--- app.js
+++ app.js
@@ -1,28 +1,4 @@
-//enum 선언 - 같은유형을 하나로 묶는 요소
-//spring enum(문자형 열거) 선언법
-var GenderType;
-(function (GenderType) {
-    GenderType["Male"] = "male";
-    GenderType["Female"] = "female";
-})(GenderType || (GenderType = {}));
-var studentID = 12345;
-var studentName = "soyoung";
-var courseCompleted = false;
-var student1 = {
-    studentID: 12345,
-    studentName: "soyoung",
-    courseCompleted: false,
-    gender: 'male'
-};
-function getStudentDetails(studentID) {
-    return {
-        studentID: 12345,
-        studentName: "soyoung",
-        courseCompleted: false,
-        gender: 'female'
-    };
-}
-//매개변수 타입으로도 사용가능한 interface
-function saveStudentDetails(student) {
-}
-saveStudentDetails(student1);
+//아무제약이 없는 any타입(자바스크립트와 동일)
+//되도록 안쓰는게 좋음
+var someValue = 5;
+someValue = 'five';
--- app.ts
+++ app.ts
@@ -1,41 +1,4 @@
-//enum 선언 - 같은유형을 하나로 묶는 요소
-//spring enum(문자형 열거) 선언법
-enum GenderType{
-  Male ='male',
-  Female='female'
-}
-
-let studentID: number = 12345;
-let studentName: string = "soyoung";
-let courseCompleted: boolean = false;
-
-let student1:Student={
-    studentID: 12345,
-    studentName: "soyoung",
-    courseCompleted: false,
-    gender: 'male'
-}
-interface Student {
-  readonly studentID: number;
-  studentName?: string; 
-  courseCompleted: boolean;
-  // gender: GenderType; enum사용
-  gender: 'male'|'female' //리터럴타입 사용
-  //addComment(comment:string):string
-  addComment?:(comment:string)=>string
-}
-
-function getStudentDetails(studentID: number): Student {
-  return {
-    studentID: 12345,
-    studentName: "soyoung",
-    courseCompleted: false,
-    gender: 'female'
-  };
-}
-
-//매개변수 타입으로도 사용가능한 interface
-function saveStudentDetails(student:Student):void{
-
-}
-saveStudentDetails(student1)
+//아무제약이 없는 any타입(자바스크립트와 동일)
+//되도록 안쓰는게 좋음
+let someValue:any=5;
+someValue='five';
