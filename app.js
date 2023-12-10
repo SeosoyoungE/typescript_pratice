d1d7ac3ecb496dfeb853286c78982eaeceddd19f
soyoung <soyseo99@naver.com>
2023-12-10 23:14:25 +0900

    type guard

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-var price;
-var phoneNumber;
+var numberPrice;
+//타입가드에 의한 타입에러
+var setPrice1 = function (price) {
+    numberPrice = price;
+};
+//if문을 통해 type확인후 실행
+var setPrice2 = function (price) {
+    if (typeof price === 'number') {
+        numberPrice = price;
+    }
+};
--- app.ts
+++ app.ts
@@ -1,6 +1,16 @@
-//type alias
-//반복되는 유니온 타입을 정의하여 코드정리
+//type guard
 type StrOrNum=string|number;
+let numberPrice:number;
 
-let price:StrOrNum;
-let phoneNumber:StrOrNum;
+//타입가드에 의한 타입에러
+const setPrice1=(price:StrOrNum)=>{
+  numberPrice=price;
+}
+
+
+//if문을 통해 type확인후 실행
+const setPrice2=(price:StrOrNum)=>{
+  if(typeof price==='number'){
+    numberPrice=price;
+}
+}
