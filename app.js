18f504c91e58c0d6c7e60f0bb61919d531d7206b
soyoung <soyseo99@naver.com>
2023-12-25 23:24:57 +0900

    Constructor

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 //class 사용법
 var user = /** @class */ (function () {
-    function user() {
+    function user(name, age, job, hourlyRate) {
         var _this = this;
         this.userInfo = function () {
             console.log("".concat(_this.name, "\uC758 \uB098\uC774\uB294 ").concat(_this.age, ", \uC9C1\uC5C5\uC740 ").concat(_this.job));
         };
+        this.name = name;
+        this.age = age;
+        this.job = job;
+        this.hourlyRate = hourlyRate;
     }
     return user;
 }());
-var user1 = new user();
-user1.age = 25;
-user1.name = 'soyoung';
+var user1 = new user('soyoung', 25, '주니어개발자', 10000);
 user1.userInfo();
--- app.ts
+++ app.ts
@@ -3,12 +3,21 @@ class user{
   name:string;
   age:number;
   job:string;
+  hourlyRate:number;
+  constructor(name:string,
+    age:number,
+    job:string,
+    hourlyRate:number){
+      this.name=name;
+      this.age=age;
+      this.job=job;
+      this.hourlyRate=hourlyRate;
+
+  }
 
   userInfo=():void=>{
     console.log(`${this.name}의 나이는 ${this.age}, 직업은 ${this.job}`)
   }
 }
-let user1=new user();
-user1.age=25;
-user1.name='soyoung'
+let user1=new user('soyoung',25,'주니어개발자',10000);
 user1.userInfo();
